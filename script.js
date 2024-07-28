f804db7a1ceec9d64fa9e66b9c2013f036685090
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2024-07-28 01:33:59 +0000

    start planning storyline (2)

--- script.js
+++ script.js
@@ -167,9 +167,18 @@ One night, you are watching Netflix home alone in your upstairs bedroom when a l
 
 OPTION 1.1 & 3.1 PATH: You shine your phone's flashlight as you go downstairs, each floor creaking loudly. You double check the lock on your front door - it was open. You look around your living room, but nothing looks out of the ordinary. (OPTION 2.1: Stay downstairs for a little, OPTION 2.2: Go back up to your room.)
 
+OPTION 2.1: You stay downstairs, sitting on your living room couch reading a book. Nothing happens for a bit, but then you hear noises, and out of the corner of your eyes, you see movement. "Hello? Who's there?" you call out. Nobody answers. You text your parents asking them when they'll be back - and after ~15 minutes, they answer that they're home and you hear a car roll into the driveway.
+(OPTION 5.1: Open the door for them, OPTION 5.2: Call them)
+
+OPTION 5.1: You open the door, and they walk in. "Mom, Dad, I was so scared, I kept hearing weird noises, ......" you ramble on and on. They turn around, and you gasp as you look at their face - blood seeping out of their eyeballs and mouth. You scream and run away, but its too late. You're next. THE END.
+
+OPTION 5.2: You call your parents, but there is no answer. That's weird, they just texted you. You call your grandma (whose house your parents were at). Your grandma say that they never came over. Starting to get really freaked out, you turn off all the lights and close the doors and windows. You text your parents number that you went to a friend's house to stay over. The next morning, you get a phone call - saying your parents died in the night. You survived, but you'll never know who you were texting then or the reason for their death. THE END.
+
 OPTION 1.2 PATH: You continue watching your show, unbothered for ~15 minutes. All of a sudden, you hear more noises - louder. (OPTION 3.1: Check out the noise, OPTION 3.2: Lock your bedroom door & windows)
 
-OPTION 3.2 PATH: A little rattled, you lock your bedroom door and windows, and text your parents asking them when they'll be back. You feel a little cold, so you walk over to your closet to grab a sweater but, in the dark of your closet, you think you see a pair of glowing yellow eyes. (OPTION 4.1: Ignore it and go back into your bed with your show. OPTION 4.2: Turn on the lights)
+OPTION 2.2 & 3.2 PATH: A little rattled, you lock your bedroom door and windows, and text your parents asking them when they'll be back. You feel a little cold, so you walk over to your closet to grab a sweater but, in the dark of your closet, you think you see a pair of glowing yellow eyes. (OPTION 4.1: Ignore it and go back into your bed with your show. OPTION 4.2: Turn on the lights)
 
 OPTION 4.1 PATH: You eventually fall asleep, with your show on play. The next morning, you wake up in a basement, chained to the floor. Shaking, you yell and scream until your voice goes hoarse. But there is nothing to be done - nobody will ever know what happened to you. THE END.
+
+OPTION 4.2: You turn on the lights, and look back at the "eyes" but it turns out it was just a light from your brother's old toys. You flop back into bed, and the noises seem to stop. You decide it was probably just you being paranoid. At around midnight, you get a knock on your door. You open the door, expecting your parents. But it's not them - it's your grandma. Tears fly down her face as she says, "It wasn't my fault, I swear, I didn't do anything!!". Your parents were eating dinner with your grandma when they dropped dead. Later it was found that the reason for death was posioning. Your grandma is your legal guardian for the rest of your life - and you will never find out what really happened that night. THE END.
 */
